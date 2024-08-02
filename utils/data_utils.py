7ee5d708caaa3f5db4e332a1f85eaa8c50f43f48
Zheng Su <suzh5@aliyun.com>
2024-08-02 13:13:23 +1000

    added google_analytics2

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -34,13 +34,12 @@ def get_gene_id(gene, gene_embeddings, ensembl_to_symbol, symbol_to_ensembl):
 
 def add_google_analytics(measurement_id):
     ga_script = f"""
-    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id={measurement_id}"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){{dataLayer.push(arguments);}}
       gtag('js', new Date());
-
       gtag('config', '{measurement_id}');
     </script>
     """
